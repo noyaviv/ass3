35d84fa0cf34181c853780bbd8acc26932270fe9
noy.aviv <noy.aviv@servicenow.com>
2021-06-03 21:35:31 +0300

    last commit

--- kernel/vm.c
+++ kernel/vm.c
@@ -188,7 +188,7 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
       panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
-    if(do_free & (*pte & PTE_PG)==0){
+    if(do_free && (*pte & PTE_PG)==0){
       printf("******im in here***** \n");
       uint64 pa = PTE2PA(*pte);
       kfree((void*)pa);
